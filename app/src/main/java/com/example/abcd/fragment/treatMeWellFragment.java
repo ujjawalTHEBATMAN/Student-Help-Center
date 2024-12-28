fd3e0307a01e03d19ba966c0a5a093678ae622dd
Ujjawal <ujjawalvishwakarma266@gmail.com>
2024-12-28 18:09:35 +0530

    after all dashboard,treatme design ok

--- app/src/main/java/com/example/abcd/fragment/treatMeWellFragment.java
+++ app/src/main/java/com/example/abcd/fragment/treatMeWellFragment.java
@@ -57,7 +57,7 @@ public class treatMeWellFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_treat_me_well, container, false);
     }
 
