d81bfe38e0b2fb5e0cfa8416152e6f84b5c14594
ujjawal <ujjawalvishwakarma266@gmail.com>
2025-01-19 02:01:46 +0530

    treatme backend completed ui updated

--- app/src/main/java/com/example/abcd/fragment/treatMeWellFragment.java
+++ app/src/main/java/com/example/abcd/fragment/treatMeWellFragment.java
@@ -6,67 +6,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
 import com.example.abcd.R;
 import com.google.android.material.button.MaterialButton;
 
 public class treatMeWellFragment extends Fragment {
-    private static final String GITHUB_SHC_URL = "https://github.com/ujjawalTHEBATMAN/Student-Help-Center";
-    private static final String GITHUB_DEV_URL = "https://github.com/ujjawalTHEBATMAN/";
-
-    public treatMeWellFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_treat_me_well, container, false);
 
+        // Project GitHub button
+        MaterialButton btnGithubSHC = view.findViewById(R.id.btnGithubSHC);
 
-        setupButton(view, R.id.btnGithubSHC, GITHUB_SHC_URL);
+        // Team member 1 buttons
+        MaterialButton btnGithub1 = view.findViewById(R.id.btnGithub1);
+        MaterialButton btnLinkedin1 = view.findViewById(R.id.btnLinkedin1);
+        MaterialButton btnLeetcode1 = view.findViewById(R.id.btnLeetcode1);
+
+        // Team member 2 buttons
+        MaterialButton btnGithub2 = view.findViewById(R.id.btnGithub2);
+        MaterialButton btnLinkedin2 = view.findViewById(R.id.btnLinkedin2);
+
+        // Team member 3 buttons
+        MaterialButton btnGithub3 = view.findViewById(R.id.btnGithub3);
+        MaterialButton btnLinkedin3 = view.findViewById(R.id.btnLinkedin3);
+
+        // Team member 4 buttons
+        MaterialButton btnGithub4 = view.findViewById(R.id.btnGithub4);
+        MaterialButton btnLinkedin4 = view.findViewById(R.id.btnLinkedin4);
+
+        // Team member 5 buttons
+        MaterialButton btnGithub5 = view.findViewById(R.id.btnGithub5);
+        MaterialButton btnLinkedin5 = view.findViewById(R.id.btnLinkedin5);
+
+        // Project GitHub link
+        btnGithubSHC.setOnClickListener(v -> openUrl("https://github.com/ujjawalTHEBATMAN/Student-Help-Center"));
 
 
-        setupDeveloperButtons(view);
+        btnGithub1.setOnClickListener(v -> openUrl("https://github.com/ujjawalTHEBATMAN"));
+        btnLinkedin1.setOnClickListener(v -> openUrl("https://www.linkedin.com/in/ujjawal-vishwakarma-aba5b6303/"));
+        btnLeetcode1.setOnClickListener(v -> openUrl("https://leetcode.com/u/ujjawalvishwakarma266/"));
+
+
+        btnGithub2.setOnClickListener(v -> openUrl("https://github.com/madara"));
+        btnLinkedin2.setOnClickListener(v -> openUrl("https://linkedin.com/in/madara"));
+
+
+        btnGithub3.setOnClickListener(v -> openUrl("https://github.com/itachi"));
+        btnLinkedin3.setOnClickListener(v -> openUrl("https://linkedin.com/in/itachi"));
+
+
+        btnGithub4.setOnClickListener(v -> openUrl("https://github.com/kakashi"));
+        btnLinkedin4.setOnClickListener(v -> openUrl("https://linkedin.com/in/kakashi"));
+
+
+        btnGithub5.setOnClickListener(v -> openUrl("https://github.com/primagan"));
+        btnLinkedin5.setOnClickListener(v -> openUrl("https://linkedin.com/in/primagan"));
 
         return view;
-    }
-
-    private void setupDeveloperButtons(View view) {
-        // GitHub buttons
-        for (int i = 1; i <= 5; i++) {
-            int githubId = getResources().getIdentifier("btnGithub" + i, "id", requireContext().getPackageName());
-            int linkedinId = getResources().getIdentifier("btnLinkedin" + i, "id", requireContext().getPackageName());
-
-            setupButton(view, githubId, GITHUB_DEV_URL);
-            setupButton(view, linkedinId, GITHUB_DEV_URL);
-
-            // Setup LeetCode button only for the first team member
-            if (i == 1) {
-                int leetcodeId = getResources().getIdentifier("btnLeetcode1", "id", requireContext().getPackageName());
-                setupButton(view, leetcodeId, GITHUB_DEV_URL);
-            }
-        }
-    }
-
-    private void setupButton(View view, int buttonId, final String url) {
-        MaterialButton button = view.findViewById(buttonId);
-        if (button != null) {
-            button.setOnClickListener(v -> openUrl(url));
-        }
     }
 
     private void openUrl(String url) {
         try {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            // Verify that there's an app that can handle this intent
-            if (intent.resolveActivity(requireActivity().getPackageManager()) != null) {
-                startActivity(intent);
-            }
+            startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
-            // Consider adding error handling or user feedback here
+            // You might want to show a toast or snackbar here to inform the user if the link cannot be opened
         }
     }
 }
